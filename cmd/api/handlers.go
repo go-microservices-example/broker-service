804777e304a09025edac3b2a6e8c778ade6dcfe8
Đặng Phước Quang <quangdangfit@mail.com>
2023-08-10 13:58:05 +0700

    frontend call broker api handle

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -44,7 +44,7 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
-	// create some json we'll send to th auth microservice
+	// create some json we'll send to the auth microservice
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
 
 	// call the service
@@ -67,15 +67,15 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		app.errorJSON(w, errors.New("invalid credentials"))
 		return
 	} else if response.StatusCode != http.StatusAccepted {
-		app.errorJSON(w, errors.New("error call auth service"))
+		app.errorJSON(w, errors.New("error calling auth service"))
 		return
 	}
 
 	// create a variable we'll read response.Body into
 	var jsonFromService jsonResponse
 
-	// decode the json from auth service
-	err = json.NewDecoder(response.Body).Decode(jsonFromService)
+	// decode the json from the auth service
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
 	if err != nil {
 		app.errorJSON(w, err)
 		return
